8a7f07fd26a49ef6cf9e0eaf2f0b55ea88fc09c8
Shiro <vicktorhugo26@gmail.com>
2021-06-19 13:30:51 +0700

    MakeBurger V4

--- script/makeBurger.js
+++ script/makeBurger.js
@@ -1,3 +1,10 @@
+let burger = {
+    bun: "reguler",
+    meat: "beef",
+    veggie: "no veggie",
+    sauce: "chilli"
+}
+
 /* BUN */
 
 function changeBun() {
@@ -6,13 +13,17 @@ function changeBun() {
         document.getElementById('wheatBot').classList.add("hidden");
         document.getElementById('regTop').classList.remove("hidden");
         document.getElementById('regBot').classList.remove("hidden");
+        burger.bun = "reguler";
     }
     else {
         document.getElementById('regTop').classList.add("hidden");
         document.getElementById('regBot').classList.add("hidden");
         document.getElementById('wheatTop').classList.remove("hidden");
         document.getElementById('wheatBot').classList.remove("hidden");
+        burger.bun = "wheat";
     }
+
+    console.log(burger);
 }
 
 /* MEAT */
@@ -24,13 +35,18 @@ function changeMeat() {
 
     if (document.getElementById('meat-beef').checked === true) {    
         document.getElementById('beef-img').classList.remove("hidden");
+        burger.meat = "beef";
     }
     else if (document.getElementById('meat-chicken').checked === true) {    
         document.getElementById('chicken-img').classList.remove("hidden");
+        burger.meat = "chicken";
     }
     else {    
         document.getElementById('fish-img').classList.remove("hidden");
+        burger.meat = "fish";
     }
+
+    console.log(burger);
 }
 
 /* SAUCE */
@@ -42,16 +58,22 @@ function changeSauce() {
 
     if (document.getElementById('sauce-mush').checked === true) {    
         document.getElementById('mush-img').classList.remove("hidden");
+        burger.sauce = "mushroom";
     }
     else if (document.getElementById('sauce-bbq').checked === true) {    
         document.getElementById('bbq-img').classList.remove("hidden");
+        burger.sauce = "BBQ";
     }
     else if (document.getElementById('sauce-chilli').checked === true) {    
         document.getElementById('chilli-img').classList.remove("hidden");
+        burger.sauce = "chilli";
     }
     else {    
         document.getElementById('cheese-img').classList.remove("hidden");
+        burger.sauce = "cheese";
     }
+
+    console.log(burger);
 }
 
 /* VEGGIE */
@@ -59,8 +81,12 @@ function changeSauce() {
 function changeVeggie() {
     if (document.getElementById('veggie-yes').checked === true) {
         document.getElementById('veggie').classList.remove("hidden");
+        burger.veggie = "with veggie";
     }
     else {
         document.getElementById('veggie').classList.add("hidden");
+        burger.veggie = "no veggie";
     }
+
+    console.log(burger);
 }
